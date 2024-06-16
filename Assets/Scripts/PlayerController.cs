a77648b3b803b24b30526e2b4bfb0b7bb30aeb3c
Tito-Olisakwe <t.olisakwe@alustudent.com>
2024-06-16 19:49:59 +0200

    9

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
-    private Rigidbody rb;
+    public int health = 5;
     private int score = 0;
+
+    private Rigidbody rb;
 
     void Start()
     {
@@ -17,7 +20,6 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
         rb.AddForce(movement * speed);
     }
 
@@ -25,19 +27,27 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Pickup"))
         {
-            other.gameObject.SetActive(false);
             score++;
             Debug.Log("Score: " + score);
+            other.gameObject.SetActive(false);
         }
         else if (other.gameObject.CompareTag("Trap"))
         {
-            other.gameObject.SetActive(false);
-            score--;
-            Debug.Log("Score: " + score);
+            health--;
+            Debug.Log("Health: " + health);
         }
         else if (other.gameObject.CompareTag("Goal"))
         {
             Debug.Log("You win!");
+        }
+    }
+
+    void Update()
+    {
+        if (health <= 0)
+        {
+            Debug.Log("Game Over!");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
