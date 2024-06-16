f9fd3715ff4bcadd1372cf5fa64956999fa22d41
Tito-Olisakwe <t.olisakwe@alustudent.com>
2024-06-17 01:14:52 +0200

    12

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,19 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     public void PlayMaze()
     {
+        if (colorblindMode.isOn)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
         SceneManager.LoadScene("maze");
     }
 
     public void QuitMaze()
     {
-        // This will not work in the Unity editor
-        Application.Quit();
-
-        // Add a debug log message to confirm the button press in the editor
         Debug.Log("Quit Game");
+        Application.Quit();
     }
 }
